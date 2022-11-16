e564c07ea122ed2f8ab8c04c9975faa216d2fe92
akasaki1211 <aumogras3@yahoo.co.jp>
2022-11-17 02:07:54 +0900

    Nested folder support

--- akScriptLauncher/menu.py
+++ akScriptLauncher/menu.py
@@ -6,53 +6,57 @@ TITLE = os.path.basename(os.path.dirname(__file__))
 DEFAULTPATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'launcherScripts')
 
 class LauncherMenu(object):
+    
+    scriptPath = DEFAULTPATH
 
     def __init__(self, *args):
-        self.create()
-
-    def create(self, *args):
+        
+        if not self.scriptPath:
+            self.scriptPath = cmds.internalVar(userScriptDir=True)
+        
         if cmds.menu(TITLE, exists=True):
             cmds.deleteUI(TITLE)
-
-        cmds.menu(TITLE, label=TITLE, parent="MayaWindow", tearOff=True, postMenuCommand=self.rebuild_item)
         
+        cmds.menu(TITLE, label=TITLE, 
+                parent="MayaWindow", tearOff=True, 
+                postMenuCommand=self.build_menu)
+
         print('{} | Create main menu.'.format(TITLE))
-        
-        #self.rebuild_item()
 
-    def rebuild_item(self, *args):
-        dirs, files = self.load_scripts()
-        
-        #print(dirs)
-        #print(files)
-
+    def build_menu(self, *args):
         cmds.menu(TITLE, e=True, deleteAllItems=True)
-        cmds.setParent(TITLE, menu=True)
+        #cmds.menuItem(parent=TITLE, divider=True)
+        self.add_menu_item(TITLE, self.scriptPath)
         
+        print('{} | Rebuild menu.'.format(TITLE))
+
+    def add_menu_item(self, parent, path, *args):
+        dirs, files = self.load_scripts(path)
+        
+        for dir in dirs:
+            label, dirPath = dir
+            dirMenu = cmds.menuItem(parent=parent, label=label, 
+                                    subMenu=True, tearOff=True)
+            self.add_menu_item(dirMenu, dirPath)
+        
+        cmds.menuItem(parent=parent, divider=True)
+
         for file in files:
             label, ext, filePath, iconPath = file
 
             if ext == '.py':
-                command = self.create_py_command(filePath)
+                cmd = self.create_py_command(filePath)
             elif ext == '.mel':
-                command = self.create_mel_command(filePath)
+                cmd = self.create_mel_command(filePath)
 
-            cmds.menuItem(label=label, command=command, image=iconPath)
+            cmds.menuItem(parent=parent, label=label, command=cmd, image=iconPath)
 
-        #cmds.menuItem(divider=True)
-        
-        print('{} | Rebuild sub menu.'.format(TITLE))
-
-    def load_scripts(self, *args):
-        scriptPath = DEFAULTPATH
-        if not scriptPath:
-            scriptPath = cmds.internalVar(userScriptDir=True)
-
+    def load_scripts(self, path, *args):
         dirs = []
         files = []
 
-        for f in os.listdir(scriptPath):
-            fullPath = os.path.join(scriptPath, f)
+        for f in os.listdir(path):
+            fullPath = os.path.join(path, f)
             if os.path.isdir(fullPath):
                 dirs.append((f, fullPath))
             else:
@@ -60,9 +64,9 @@ class LauncherMenu(object):
                 if not ext in ['.py', '.mel']:
                     continue
 
-                iconPath = os.path.join(scriptPath, base + '.ico')
+                iconPath = os.path.join(path, base + '.ico')
                 if not os.path.isfile(iconPath):
-                    iconPath = os.path.join(scriptPath, base + '.png')
+                    iconPath = os.path.join(path, base + '.png')
                 if not os.path.isfile(iconPath):
                     iconPath = ''
                 
@@ -70,21 +74,20 @@ class LauncherMenu(object):
 
         return dirs, files
 
-    def create_mel_command(self, filePath, *args):
+    def create_mel_command(self, filePath, exec=True, *args):
         filePath = os.path.normpath(filePath)
         filePath = filePath.replace('\\','\\\\\\\\')
-        command = 'from maya import mel\n'
-        command += 'mel.eval(\'source \"{}\"\')'.format(filePath)
+        cmd = 'from maya import mel\n'
+        cmd += 'mel.eval(\'source \"{}\"\')'.format(filePath)
 
-        # for AriTools
-        baseName = os.path.basename(filePath)
-        if baseName.startswith('Ari'):
-            command += '\n'
-            command += 'mel.eval(\'{}();\')'.format(os.path.splitext(baseName)[0])
+        if exec:
+            name = os.path.splitext(os.path.basename(filePath))[0]
+            cmd += '\nmel.eval(\'{}();\')'.format(name)
         
-        return command
+        return cmd
 
     def create_py_command(self, filePath, *args):
-        command = 'from {} import run\n'.format(TITLE)
-        command += 'run.execfile(r\'{}\')'.format(filePath)
-        return command
+        cmd = 'from {} import run\n'.format(TITLE)
+        cmd += 'run.execfile(r\'{}\')'.format(filePath)
+
+        return cmd
--- userSetup.py
+++
@@ -1,7 +0,0 @@
-"""
-How to install
-	1. Place akScriptLauncher folder in PYTHONPATH.
-    2. Execute the following command in userSetup.py.
-"""
-
-import akScriptLauncher
