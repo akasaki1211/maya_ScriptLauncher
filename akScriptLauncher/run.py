d55138c39e258bfa7500cb7ae67256b00930c875
Hiroyuki Akasaki <42388721+akasaki1211@users.noreply.github.com>
2023-07-18 14:41:10 +0900

    fix(run): indent error in execfile()

--- akScriptLauncher/run.py
+++ akScriptLauncher/run.py
@@ -3,4 +3,4 @@
 def execfile(filePath, globals=None, locals=None):
     if globals is None:
         globals = {'__name__': '__main__'}
-        exec(compile(open(filePath, 'rb').read(), filePath, 'exec'), globals, locals)
+    exec(compile(open(filePath, 'rb').read(), filePath, 'exec'), globals, locals)
